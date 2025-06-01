4fdbdf82d004c85d7128879b846f28a009a123af
BenLaurense <benlaurense@gmail.com>
2025-06-01 20:49:13 +0100

    package-ify some more + enable jupyter

--- fontcap_scraper/__init__.py
+++ fontcap_scraper/__init__.py
@@ -1,3 +1,15 @@
 """
 Top-level package for fontcap_scraper
 """
+
+from main import run_scraper
+from config import FontcapConfig
+from .sources import GoogleFontsSource
+from .utils import render_glyphs, save_glyphs, font_hash
+
+__all__ = [
+    "run_scraper",
+    "FontcapConfig",
+    "GoogleFontsSource", 
+    "render_glyphs", "save_glyphs", "font_hash"
+]
