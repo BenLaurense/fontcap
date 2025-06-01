013eabc63e5ba66b361ac2adf8f3a8cfb308e219
BenLaurense <benlaurense@gmail.com>
2025-06-01 20:45:15 +0100

    package-ify the training module

--- fontcap_model/__init__.py
+++ fontcap_model/__init__.py
@@ -1,3 +1,15 @@
 """
 Top-level package for fontcap_model
 """
+
+from dataset import FontcapDataset, get_dataloaders
+from train_cnn_autoencoder import train_autoencoder
+from .utils import plot_losses, display_reconstructions
+from .models import CNN_Autoencoder
+
+__all__ = [
+    "FontcapDataset", "get_dataloaders",
+    "train_autoencoder",
+    "plot_losses", "display_reconstructions",
+    "CNN_Autoencoder"
+]
