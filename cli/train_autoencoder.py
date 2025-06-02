7d5749c13181b8f426e8199bf8df56ee8dcae812
BenLaurense <benlaurense@gmail.com>
2025-06-02 23:47:26 +0100

    better handling of options in click commands

--- cli/train_autoencoder.py
+++ cli/train_autoencoder.py
@@ -3,10 +3,15 @@ import click
 from pathlib import Path
 from fontcap_model import train_cnn_autoencoder
 
+"""
+Click command for training the autoencoder. Usage (from project root with venv):
+python -m cli.train_autoencoder -dr "data/fonts" -ep 2 -bs 32 -lr 0.001 -chkdir "./something" -chki 1 -pi 1
+"""
 logging.basicConfig(
     level=logging.INFO,  # or DEBUG, WARNING, etc.
     format='[%(asctime)s] %(levelname)s: %(message)s',
 )
+
 
 @click.command()
 @click.option('--verbose', '-v', is_flag=True, help='Enable debug logging.')
@@ -14,7 +19,7 @@ logging.basicConfig(
 @click.option('--epochs', '-ep', type=int, required=True, help='Number of training epochs')
 @click.option('--batch_size', '-bs', type=int, required=True, help='Batch size per epoch')
 @click.option('--learning_rate', '-lr', type=float, required=True, help='Learning rate')
-@click.option('--checkpoint_dir', '-chkdir', type=click.Path(exists=True), required=False,
+@click.option('--checkpoint_dir', '-chkdir', type=click.Path(), required=False,
               default=Path("./checkpoints"), help='Number of training epochs')
 @click.option('--checkpoint_interval', '-chki', type=int, required=False,
               default=20, help='Save the model parameters every x epochs')
@@ -23,7 +28,8 @@ logging.basicConfig(
 @click.option('--start_state', '-st', type=click.Path(exists=True), required=False,
               help='Starting parameters file (stored in checkpoints dir')
 @click.option('--resume_loss', '-rl', is_flag=True, help='Resume loss curve')
-def run(data_root, epochs, batch_size, learning_rate, checkpoint_dir, checkpoint_interval, plot_interval, start_state, resume_loss, verbose):
+def run(data_root, epochs, batch_size, learning_rate, checkpoint_dir, checkpoint_interval, plot_interval, start_state,
+        resume_loss, verbose):
     if verbose:
         logging.getLogger().setLevel(logging.DEBUG)
     train_cnn_autoencoder(
@@ -37,6 +43,7 @@ def run(data_root, epochs, batch_size, learning_rate, checkpoint_dir, checkpoint
         state_dict_name=start_state,
         resume_loss=resume_loss)
     return
+
 
 if __name__ == '__main__':
     run()
