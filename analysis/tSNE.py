1332567f2a4cd45cd5f8593cdac3b5d438b34bba
BenLaurense <benlaurense@gmail.com>
2025-06-07 16:25:56 +0100

    propagate changes to jupyters

--- analysis/tSNE.py
+++ analysis/tSNE.py
@@ -24,13 +24,12 @@ def run_tsne(
 
 def plot_tsne_2d(latents, colours=None):
     fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111,)
     ax.scatter(latents[:, 0], latents[:, 1], c=colours, cmap='tab20', s=5, alpha=0.7)
-    ax.title('t-SNE Projection of Latents')
-    ax.xlabel('Feature 1')
-    ax.ylabel('Feature 2')
+    ax.set_title('t-SNE Projection of Latents')
+    ax.set_xlabel('Feature 1')
+    ax.set_ylabel('Feature 2')
     ax.grid(True)
-    ax.tight_layout()
     return fig
 
 
@@ -45,7 +44,6 @@ def plot_tsne_3d(latents, colours=None):
     ax.set_xlabel('Feature 1')
     ax.set_ylabel('Feature 2')
     ax.set_zlabel('Feature 3')
-    plt.tight_layout()
     return fig
 
 
@@ -57,24 +55,21 @@ def plot_tsne_2d_restricted(
     """Plots latents with labels"""
     # Indexes of latents/labels to plot
     idx = [i for i, label in enumerate(latent_labels) if label in labels_to_plot]
-    # Restrict the data
-    labels = [l for i, l in enumerate(latent_labels) if i in idx]
     allowed_latents = latents[idx, :]
-
+    allowed_latent_labels = [label for i, label in enumerate(latent_labels) if i in idx] # ['a', 'b']
     # Get the colours
-    colours = plt.cm.tab20([cl for cl in idx])
-
-    # Get each distinct label colour for the legent
-    distinct_idx = [labels.index(label) for label in labels_to_plot]
-    legend_elts = [Patch(facecolor=colours[i], label=labels[i]) for i in distinct_idx]
+    colour_encoding = [labels_to_plot.index(label) for label in allowed_latent_labels] # [0, 1]
+    colours = plt.cm.tab20(colour_encoding)
+    # Get each distinct label colour for the legend
+    distinct_idx = [allowed_latent_labels.index(label) for label in labels_to_plot]
+    legend_elts = [Patch(facecolor=colours[i], label=allowed_latent_labels[i]) for i in distinct_idx]
 
     fig = plt.figure(figsize=(10, 8))
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(111)
     ax.scatter(allowed_latents[:, 0], allowed_latents[:, 1], c=colours, s=5, alpha=0.7)
-    ax.title('t-SNE Projection of Latents')
-    ax.xlabel('Feature 1')
-    ax.ylabel('Feature 2')
+    ax.set_title('t-SNE Projection of Latents')
+    ax.set_xlabel('Feature 1')
+    ax.set_ylabel('Feature 2')
     ax.legend(handles=legend_elts, title="Legend")
     ax.grid(True)
-    ax.tight_layout()
     return fig
